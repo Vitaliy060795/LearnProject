5ad3eac504b15b3fe022df5e53be45907d1b38fe
Vitaliy <vitaliy060795@gmail.com>
2019-07-21 23:10:44 +0300

    first commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,8 +10,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("2");
-            Console.Read();
+            Console.WriteLine(AddNambers(4, 10));
+        }
+        static int AddNambers(int a, int b)
+        {
+            return 14;
         }
     }
 }
