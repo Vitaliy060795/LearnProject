04aef2fa300725c3ecc7a1defd61a3dcf2ccd15a
Vitaliy <vitaliy060795@gmail.com>
2019-07-21 20:23:10 +0300

    Revert "wrong commit"
    This reverts commit 1d35cb5c8cb9a8d3f24448d57bf0336b00206994.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("loh");
+            Console.WriteLine("2");
             Console.Read();
         }
     }
