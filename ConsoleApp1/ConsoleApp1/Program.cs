ddb41381cb3b5560f022de28df12890c87275877
Vitalii Sukhomlyn <vsukhomlyn@amcbridge.com>
2019-07-31 12:54:42 +0300

    AddNewMethod

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -16,12 +16,17 @@ namespace ConsoleApp1
             var width = Console.WindowWidth;
             string text = "Find the n - th Fibonacci number";
             var padding = width / 2 + text.Length / 2;
-            Console.WriteLine("{0," + padding + "}", text);
-            Console.WriteLine();
-            Console.WriteLine("Enter integer values ​​to determine the order of the Fibonacci number: ");
+            Console.WriteLine("{0," + padding + "}", text + '\n');
+            Stopwatch stopWatch = new Stopwatch();
+            Console.WriteLine("Enter integer values ​​to determine the order of the Fibonacci number using recursion: ");
             int.TryParse(Console.ReadLine(), out int num);
-            int temp = fib(num);
-            Console.WriteLine(temp);
+            stopWatch.Start();
+            //int temp = fib(num);
+            int temp = fib_n(4);
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            Console.WriteLine(string.Format("Fibonacci number of order {0} = {1}" + '\n' + "Elapsed time {1:00}:{2:00}:{3:00}.{4:00}", num, ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10));
             Console.Read();
         }
 
@@ -29,6 +34,20 @@ namespace ConsoleApp1
         {
             if (n < 2 ) return n;
             return fib(n - 1) + fib(n - 2);
+        }
+
+       static int fib_n(int n)
+        {
+            //F(n)
+            int x = 1;
+            //F(n-1)
+            int y = 0;
+            for (int i = 0; i < n; i++)
+            {
+                x += y;
+                y = x - y;
+            }
+            return y;
         }
 
     }   
